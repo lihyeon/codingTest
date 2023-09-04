de59a76bd11515dcd146d6f43cf0301da1b5b7b6
lihyeon <79856553+lihyeon@users.noreply.github.com>
2023-09-04 12:07:38 +0900

    [Silver III] Title: 피보나치 함수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -11,8 +11,7 @@ def fibo(n):
         for i in range(length, n+1):
             zero.append(zero[i-1]+zero[i-2])
             one.append(one[i-1]+one[i-2])
-    return zero[n], one[n]
+    print('{} {}'.format(zero[n], one[n]))
 
 for i in range(t):
-    a,b = fibo(int(input()))
-    print(a, b)
+    fibo(int(input()))
